5bee674e53797ceaeef1fc647940951bc3fff9dd
gabe.semac@uleth.ca <gabe.semac@uleth.ca>
2017-07-10 21:58:47 -0600

    Added StandardGravity to Physics namespace

---
+++ HVN3 Engine/PhysicsUtils.cc
@@ -0,0 +1,13 @@
+#include "PhysicsUtils.h"
+
+namespace hvn3 {
+	namespace Physics {
+
+		float StandardGravity() {
+
+			return 9.80665f;
+
+		}
+
+	}
+}
---
+++ HVN3 Engine/PhysicsUtils.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace hvn3 {
+	namespace Physics {
+
+		// Returns the standard acceleration due to gravity on Earth in m/s. 
+		float StandardGravity();
+
+	}
+}
