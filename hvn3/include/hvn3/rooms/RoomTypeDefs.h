051ff704885b0edf618d4a9dd2b10178a9b16376
Gabe Semac <gabe.semac@uleth.ca>
2018-09-09 18:54:52 -0600

    Added make_room function

--- hvn3/include/hvn3/rooms/RoomTypeDefs.h
+++ hvn3/include/hvn3/rooms/RoomTypeDefs.h
@@ -4,6 +4,7 @@
 namespace hvn3 {
 
 	class IRoom;
+	class Room;
 
 	typedef int RoomId;
 	typedef std::shared_ptr<IRoom> RoomPtr;
@@ -11,5 +12,14 @@ namespace hvn3 {
 	enum : RoomId {
 		NULL_ROOM_ID = -1
 	};
+
+	template<typename room_type = Room, typename... Args>
+	RoomPtr make_room(Args&&... args) {
+
+		RoomPtr ptr = std::make_shared<room_type>(std::forward<Args>(args)...);
+
+		return ptr;
+
+	}
 
 }
