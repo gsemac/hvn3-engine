de5751871459fccbab59af8f6584bf97ee2759ae
gsemac <gabe.semac@gmail.com>
2019-04-08 22:59:17 -0600

    Added UniqueIntegerGenerator class

---
+++ hvn3/include/hvn3/utility/UniqueIntegerGenerator.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include "hvn3/exceptions/Exception.h"
+
+#include <cassert>
+#include <queue>
+#include <limits>
+
+namespace hvn3 {
+
+	template<typename IntegerType>
+	class UniqueIntegerGenerator {
+
+	public:
+		typedef IntegerType integer_type;
+
+		UniqueIntegerGenerator() = default;
+		UniqueIntegerGenerator(integer_type startingValue);
+
+		integer_type Next();
+		void Free(integer_type value);
+
+	private:
+		integer_type _current_value;
+		std::queue<integer_type> _freed_values;
+
+	};
+
+	template<typename IntegerType>
+	UniqueIntegerGenerator<IntegerType>::UniqueIntegerGenerator(typename UniqueIntegerGenerator<IntegerType>::integer_type startingValue) {
+		_current_value = startingValue;
+	}
+	template<typename IntegerType>
+	typename UniqueIntegerGenerator<IntegerType>::integer_type UniqueIntegerGenerator<IntegerType>::Next() {
+
+		if (_freed_values.size() > 0) {
+
+			// If there are values we can recycle from the pool, return one of those.
+
+			integer_type value = _freed_values.front();
+
+			_freed_values.pop();
+
+			return value;
+
+		}
+		else {
+
+			// Make sure we haven't exhausted the entire unique range.
+
+			if (_current_value >= std::numeric_limits<integer_type>::max())
+				throw System::Exception("Unique range has been exhausted.");
+
+			// Return the next value.
+			return _current_value++;
+
+		}
+
+	}
+	template<typename IntegerType>
+	void UniqueIntegerGenerator<IntegerType>::Free(integer_type value) {
+
+		// The value we're freeing should be one that was returned previously.
+		assert(value < _current_value);
+
+		_freed_values.push(value);
+
+	}
+
+}
