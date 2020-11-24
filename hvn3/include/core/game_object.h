0e29beab219454c8361db2dae2dc7ec57de91b75
gsemac <gabe.semac@gmail.com>
2020-11-23 17:17:11 -0800

    Added IGameObject and GameObject classes

---
+++ hvn3/include/core/game_object.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "core/igame_object.h"
+
+namespace hvn3::core {
+
+	class GameObject :
+		public IGameObject {
+	};
+
+}
---
+++ hvn3/include/core/igame_object.h
@@ -0,0 +1,12 @@
+#pragma once
+
+namespace hvn3::core {
+
+	class IGameObject {
+
+	public:
+		virtual ~IGameObject() = default;
+
+	};
+
+}
